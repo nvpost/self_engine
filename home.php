7c26b57db6597716a20f823b803942be350dc800
nvpost <nvbpost@yandex.ru>
2021-02-19 17:02:28 +0300

    'htac'

--- home.php
+++ home.php
@@ -3,15 +3,36 @@ require './cats.php';
 
 //deb($rootCats);
 
+print_r($_GET);
+
 foreach($rootCats as $key => $rootCat){
-    deb($rootCat);
+    drowShowCaseItem($rootCat);
 }
+
 
 
 
 
 function drowShowCaseItem($item){
-   $itemHtml = "<div class='show_case_item'>";
+    global $imgUrl;
+    $itemHtml = "<div class='show_case_item'>";
+        $itemHtml .= "<div class='show_case_item_header'>";
+            $itemHtml .= "<h2>".$item['label']."</h2>";
+        $itemHtml ."</div>";
+
+        $itemHtml .= "<div class='show_case_item_img'>";
+            $itemHtml .= "<img src='https://picsum.photos/400/200'>";
+            //$itemHtml .= "<h2>".$item['img']."</h2>";
+        $itemHtml ."</div>";
+
+        $itemHtml .= "<div class='show_case_item_actions'>";
+            $itemHtml .= "<a href='./category/".$item['cat_id']."'>Перейти к категории ".$item['cat_id']."</a>";
+        $itemHtml ."</div>";
+
+   $itemHtml ."</div>";
+
+    echo $itemHtml;
+    //deb($item);
 }
 
 
@@ -21,11 +42,18 @@ function drowShowCaseItem($item){
 
 ?>
 
-<div class="show_case_item">
-    <h3>$item['label']</h3>
-</div>
+
 
 
 <?php
 require './debug_line.php';
 ?>
+
+<script>
+    function getImg(){
+        let res = await fetch('https://picsum.photos/400/200')
+        console.log(res.url)
+        return res.url
+    }
+
+</script>
